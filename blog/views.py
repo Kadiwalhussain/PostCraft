d9912b9e62f3d702c746cbf3c3751a6867a722ba
Hussain <hussainkadiwal100@gmail.com>
2025-08-17 12:21:38 +0530

    Initial commit - PostCraft Django blog project

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import Http404
+
+from .models import Post
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    try:
+        post = Post.published.filter(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post Found.")
+    return render(request, 'blog/post/detail.html', {'post': post})
