09599d7a921fcf573916080dcf8cc48530e64483
Hussain <hussainkadiwal100@gmail.com>
2025-08-18 16:17:16 +0530

    Initial commit - PostCraft Django blog project

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
-from django.http import Http404
+# blog/views.py
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Post
 
+
+from django.core.paginator import Paginator
+from django.shortcuts import render
 from .models import Post
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()  # get all published posts
+    paginator = Paginator(object_list, 3)  # 👈 3 posts per page
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
-    try:
-        post = Post.published.filter(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post Found.")
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=slug,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     return render(request, 'blog/post/detail.html', {'post': post})
